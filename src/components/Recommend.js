c97e2c4172f121b3ebee6fffc12a22def37819b9
RichardKullu <35299116+RichardKullu@users.noreply.github.com>
2022-11-08 11:49:01 +0530

    Add files via upload

---
+++ src/components/Recommend.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { useState, useEffect } from 'react-router-dom';
+
+const Recommend = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Recommend
---
+++ src/components/Top.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { useState, useEffect } from "react";
+
+const Top = () => {
+  const [data, setData] = useState("");
+  const URL = "http://localhost:1337/api/categories";
+
+  useEffect(() => {
+    fetch(URL)
+      .then((resp) => {
+        if (!resp.ok) {
+          throw new Error("Error");
+        }
+        return resp.json();
+      })
+      .then((item) => {
+        const temp = item.data.map((item) => {
+          return {
+            id: item.attributes.ctgy_id,
+            name: item.attributes.ctgy_name,
+            slug: item.attributes.ctgy_slug,
+          };
+        });
+        setData(temp);
+        console.log(temp);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  return (
+    <div>
+      <section className="home" id="home" style={{marginBottom: '1rem'}}>
+        <div className="home-text container">
+          <p className="home-title fw-bold">University of Delhi</p>
+          <span className="home-subtitle">News Portal</span>
+        </div>
+      </section>
+
+      <div class="post-filter container" >
+            <span class="btn" data-toggle="portfilter" data-target="all">
+                <div className="text-white">All</div>
+            </span>
+            { data && 
+                data.map(item => {
+                    return(
+                        <span class="btn" data-toggle="portfilter" data-target={item.slug}>
+                            <div className="text-white">{item.name}</div>
+                        </span>
+                    );
+                })
+            }
+        </div>
+      
+    </div>
+  );
+};
+
+export default Top;
