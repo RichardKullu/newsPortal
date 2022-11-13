273f34fc628c3e36d8d3d4d56c38d650d560f3a9
RichardKullu <35299116+RichardKullu@users.noreply.github.com>
2022-11-14 03:08:17 +0530

    Update ImageEmbed.js

--- src/components/ImageEmbed.js
+++ src/components/ImageEmbed.js
@@ -2,21 +2,6 @@ import React from 'react';
 import ImageGallery from 'react-image-gallery';
 import "../../node_modules/react-image-gallery/styles/css/image-gallery.css";
 
-const images = [
-  {
-    original: 'https://picsum.photos/id/1018/1000/600/',
-    thumbnail: 'https://picsum.photos/id/1018/250/150/',
-  },
-  {
-    original: 'https://picsum.photos/id/1015/1000/600/',
-    thumbnail: 'https://picsum.photos/id/1015/250/150/',
-  },
-  {
-    original: 'https://picsum.photos/id/1019/1000/600/',
-    thumbnail: 'https://picsum.photos/id/1019/250/150/',
-  },
-];
-
 const ImageEmbed = (props) => {
   const img = props.url.data.map(item => {
       return {
